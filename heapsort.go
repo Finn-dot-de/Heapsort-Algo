3044f089a8d7b57dba3cefc52c5df6c8f3f29cdb
LY04 <Finn.Laskowski@bamf.bund.de>
2024-04-17 10:04:49 +0200

    änderung

--- heapsort.go
+++ heapsort.go
@@ -19,7 +19,7 @@ func main() {
 	fmt.Printf("Unsortierte liste: %v <<<<<<<<\n\n", numbers)
 
 	// Aufruf der Sortier Funktion
-	numbers = heap_sort(numbers)
+	numbers = Heap_sort(numbers)
 
 	// Überprüft, ob die Liste sortiert ist
 	for i := 1; i < len(numbers); i++ {
@@ -35,17 +35,16 @@ func main() {
 }
 
 // Initialisierung: Sie initialisiert den größten Index als Wurzelindex und berechnet den Index des linken und rechten Kindes.
-// 
+//
 // Überprüfung der Kinder: Sie überprüft, ob das linke oder rechte Kind größer ist als die Wurzel. Wenn ja, wird der Index des größten Kindes als der neue größte Index festgelegt.
-// 
+//
 // Tauschen der Elemente: Wenn die Wurzel nicht das größte Element ist (d.h., eines der Kinder ist größer), tauscht sie die Wurzel mit dem größten Element.
-// 
+//
 // Rekursiver Aufruf: Nach dem Tauschen könnte der getauschte Knoten die Heap-Eigenschaft verletzen.
 // Daher ruft die Funktion sich selbst rekursiv auf, um sicherzustellen, dass der getauschte Knoten und seine Kinder die Heap-Eigenschaft erfüllen.
-// 
+//
 // Rückgabe des Heaps: Schließlich gibt die Funktion den modifizierten Heap zurück.
-// 
-func heapify(heap []int, heap_size int, root_index int) []int {
+func Heapify(heap []int, heap_size int, root_index int) []int {
 	// Initialisiert den größten Index als Wurzelindex
 	largest := root_index
 	// Berechnet den Index des linken und rechten Kindes
@@ -64,23 +63,22 @@ func heapify(heap []int, heap_size int, root_index int) []int {
 	if largest != root_index {
 		heap[root_index], heap[largest] = heap[largest], heap[root_index]
 		// Heapify den neuen größten Knoten
-		heap = heapify(heap, heap_size, largest)
+		heap = Heapify(heap, heap_size, largest)
 	}
 	return heap
 }
 
 // Prüft die Länge der unsortierten Liste: Wenn die Liste nur ein Element oder kein Element enthält, wird sie als sortiert betrachtet und sofort zurückgegeben.
-// 
-// Erstellt einen Heap aus der unsortierten Liste: Ein Heap ist eine spezielle Baumstruktur, in der jedes Elternelement größer oder gleich seinen Kindern ist. 
-// Dies wird erreicht, indem die heapify Funktion auf jedes Element der Liste angewendet wird, beginnend in der Mitte der Liste und rückwärts gehend.
-// 
-// Sortiert den Heap: Die heap_sort Funktion entfernt dann das größte Element (die Wurzel des Heaps) 
-// und tauscht es mit dem letzten Element des Heaps. 
+//
+// Erstellt einen Heap aus der unsortierten Liste: Ein Heap ist eine spezielle Baumstruktur, in der jedes Elternelement größer oder gleich seinen Kindern ist.
+// Dies wird erreicht, indem die Heapify Funktion auf jedes Element der Liste angewendet wird, beginnend in der Mitte der Liste und rückwärts gehend.
+//
+// Sortiert den Heap: Die Heap_sort Funktion entfernt dann das größte Element (die Wurzel des Heaps)
+// und tauscht es mit dem letzten Element des Heaps.
 // Dieses Element wird nun als sortiert betrachtet und aus dem Heap entfernt.
-// 
+//
 // Der Heap wird dann erneut “geheapified”, und der Prozess wird wiederholt, bis der gesamte Heap sortiert ist.
-// 
-func heap_sort(unsorted_list []int) []int {
+func Heap_sort(unsorted_list []int) []int {
 	// Überprüft, ob die Liste leer oder nur ein Element enthält
 	if len(unsorted_list) <= 1 {
 		return unsorted_list
@@ -89,12 +87,12 @@ func heap_sort(unsorted_list []int) []int {
 	heap_size := len(unsorted_list)
 	// Baut den Max-Heap auf
 	for index := heap_size/2 - 1; index >= 0; index-- {
-		unsorted_list = heapify(unsorted_list, heap_size, index)
+		unsorted_list = Heapify(unsorted_list, heap_size, index)
 	}
-	// Tauscht das erste Element mit dem letzten Element und heapify den reduzierten Heap
+	// Tauscht das erste Element mit dem letzten Element und Heapify den reduzierten Heap
 	for i := heap_size - 1; i > 0; i-- {
 		unsorted_list[0], unsorted_list[i] = unsorted_list[i], unsorted_list[0]
-		unsorted_list = heapify(unsorted_list, i, 0)
+		unsorted_list = Heapify(unsorted_list, i, 0)
 	}
 	return unsorted_list
 }
